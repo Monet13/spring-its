aa541665868d142e10ce70a3aefc87c914b001d2
Monet13 <kerr13.2018@gmail.com>
2024-03-10 21:29:45 +0900

    first commit

---
+++ src/main/java/com/example/demo/web/issue/IssueController.java
@@ -0,0 +1,49 @@
+package com.example.demo.web.issue;
+
+import com.example.demo.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/issues")
+@RequiredArgsConstructor
+public class IssueController {
+
+    private final IssueService issueService;
+
+    // GET /issues
+    @GetMapping
+    public String showList(Model model) {
+        model.addAttribute("issueList",issueService.findAll());
+        return "issues/list";
+    }
+
+    // GET issues/creationForm
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute IssueForm form) {
+        return "issues/creationForm";
+    }
+
+    // POST /issues
+    @PostMapping
+    public String create(@Validated IssueForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return showCreationForm(form);
+        }
+        issueService.create(form.getSummary(), form.getDescription());
+        return "redirect:/issues";
+    }
+
+    // GET /issues/{issueId}
+    @GetMapping("/{issueId}")
+    public String showDetail(@PathVariable("issueId") long issueId, Model model) {
+        model.addAttribute("issue", issueService.findById(issueId));
+        return "issues/detail";
+    }
+}
